0028151a3f5956a343f6efb74998d5ed6ea3c821
iChissingue <ichissingue@outlook.com>
2023-07-11 17:49:37 +0200

    Adicionar estrutura inicial do projeto

---
+++ src/Pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Header from "../Partials/Header"
+
+
+const Home = ()=>{
+
+
+    return (
+        <> 
+            <Header/>
+            <h1>Home</h1>
+            <p>Este e meu primeiro saite</p>
+        </>
+    )
+}
+
+export default Home
